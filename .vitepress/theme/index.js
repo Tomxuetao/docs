c5bf5ddae8c509724e5477e60109b8f7b7b85985
Tomxuetao <wangxt@citycloud.com.cn>
2023-05-29 14:08:51 +0800

    feat(components): 项目初始化

---
+++ .vitepress/theme/index.js
@@ -0,0 +1,14 @@
+// https://vitepress.dev/guide/custom-theme
+import { h } from 'vue'
+import Theme from 'vitepress/theme'
+import './style.css'
+
+export default {
+  ...Theme,
+  Layout: () => {
+    return h(Theme.Layout, null, {
+    })
+  },
+  enhanceApp({ app, router, siteData }) {
+  }
+}
