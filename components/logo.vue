0631034f0ca84f966ebf618452c0a8781d92d342
Tomxuetao <15555030339@163.com>
2023-09-14 10:58:02 +0800

    切换工具

---
+++ components/logo.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="logo-wrap">
+    <img class="logo-img" src="/img/logo.webp" alt="">
+    <div class="logo-text">
+      Tomxuetao
+    </div>
+  </div>
+</template>
+
+<style>
+.logo-wrap {
+  height: 64px;
+  display: flex;
+  align-items: center;
+
+  .logo-img {
+    width: 28px;
+    height: 28px;
+    border-radius: 50%;
+    object-fit: contain;
+  }
+
+  .logo-text {
+    font-size: 16px;
+    font-weight: 600;
+    margin-left: 12px;
+  }
+}
+</style>
