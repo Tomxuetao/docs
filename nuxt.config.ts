25ad005523067f029915c28b01b0a5a0e160d2cc
Tomxuetao <15555030339@163.com>
2023-09-15 09:29:01 +0800

    升级nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,4 @@
 export default defineNuxtConfig({
-  // https://github.com/nuxt-themes/docus
   extends: '@nuxt-themes/docus',
-
-  modules: [
-    // https://github.com/nuxt-modules/plausible
-    '@nuxtjs/plausible',
-    // https://github.com/nuxt/devtools
-    '@nuxt/devtools'
-  ]
+  modules: ['@nuxtjs/plausible', '@nuxt/devtools']
 })
