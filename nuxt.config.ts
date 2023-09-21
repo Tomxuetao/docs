beae266aaa71d914d8a2c4939c285a879712a78a
Tomxuetao <15555030339@163.com>
2023-09-21 11:39:16 +0800

    添加algolia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,19 @@
 export default defineNuxtConfig({
   extends: '@nuxt-themes/docus',
-  modules: ['@nuxtjs/plausible', '@nuxt/devtools']
+  modules: ['@nuxtjs/plausible', '@nuxt/devtools'],
+  runtimeConfig: {
+    public: {
+      plausible: {
+        domain: 'tomxuetao.eu.org',
+      },
+      algolia: {
+        applicationId: '0XMIMAT23T',
+        apiKey: '49706735aa647d9c385da14759ce62d9',
+        langAttribute: 'lang',
+        docSearch: {
+          indexName: 'tomxuetao'
+        }
+      }
+    }
+  }
 })
